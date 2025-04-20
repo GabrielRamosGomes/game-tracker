f128dde63cd65dbef6209594c88eedcb8378101b
rapilukz <gabrielramosgomes82@gmail.com>
2025-04-20 21:10:58 +0100

    ran format

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,3 +1,2 @@
 // import { pgTable, serial, text, date, boolean, timestamp, pgEnum } from 'drizzle-orm/pg-core'
 // export const roleEnum = pgEnum('roles', ['Admin', 'Standard'])
-
--- server/utils/igdb.ts
+++ server/utils/igdb.ts
@@ -1,42 +1,42 @@
 class IGDB_Client {
-    private client_id: string;
-    private access_token: string;
+    private client_id: string
+    private access_token: string
 
-    private base_url: string = "https://api.igdb.com/v4";
+    private base_url: string = 'https://api.igdb.com/v4'
 
     constructor(client_id: string, access_token: string) {
-        this.client_id = client_id;
-        this.access_token = access_token;
+        this.client_id = client_id
+        this.access_token = access_token
     }
 
     private get headers() {
         return {
-            "Client-ID": this.client_id,
+            'Client-ID': this.client_id,
             Authorization: `Bearer ${this.access_token}`,
-            "Content-Type": "application/json",
-        };
+            'Content-Type': 'application/json'
+        }
     }
 
     private async request<T>(endpoint: string, body: string): Promise<T> {
         const response = await $fetch(`${this.base_url}/${endpoint}`, {
-            method: "POST",
+            method: 'POST',
             headers: this.headers,
-            body: body,
-        });
+            body: body
+        })
 
         if (!response.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
+            throw new Error(`Error: ${response.status} ${response.statusText}`)
         }
 
-        return response.json();
+        return response.json()
     }
 }
 
 const igbd_client = new IGDB_Client(
     process.env.IGBD_CLIENT_ID as string,
     process.env.IGBD_ACCESS_TOKEN as string
-);
+)
 
 export function useIGBD() {
-    return igbd_client;
+    return igbd_client
 }
