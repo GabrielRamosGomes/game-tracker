73c9f3f0d095399fe962c13d0241fa42fcb8cbf1
rapilukz <gabrielramosgomes82@gmail.com>
2025-04-20 21:05:36 +0100

    added workflows

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,9 +1,9 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
-import * as schema from "../database/schema";
+import { drizzle } from 'drizzle-orm/node-postgres'
+import pg from 'pg'
+import * as schema from '../database/schema'
 
-const pool = new pg.Pool( {
-    connectionString: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+const pool = new pg.Pool({
+    connectionString: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`
 })
 
 export function useDrizzle() {
